b01b279698e84ec74e548842d74516de2426703b
VincentGonnet <vincent.gonnet08@gmail.com>
2023-07-05 18:04:00 +0200

    feat: simple movements

---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    // private RaycastHit2d hit;
+
+    // void FixedUpdate()
+    // {
+    //     hit = Physics2D.Raycast(transform.position, transform.right, 1f);
+    //     Debug.DrawRay(transform.position, transform.right, Color.red);
+    // }
+}
