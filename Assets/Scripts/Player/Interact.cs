f1ba96c9503fbbdc1da1333085a703e4234a3392
VincentGonnet <vincent.gonnet08@gmail.com>
2023-07-06 14:36:40 +0200

    feat: player look rotation

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    // private RaycastHit2d hit;
+    [SerializeField] LayerMask interactableLayer;
+    [SerializeField] float interactDistance = 1f;
+    private RaycastHit2D hit;
 
-    // void FixedUpdate()
-    // {
-    //     hit = Physics2D.Raycast(transform.position, transform.right, 1f);
-    //     Debug.DrawRay(transform.position, transform.right, Color.red);
-    // }
+    void FixedUpdate()
+    {
+        hit = Physics2D.Raycast(transform.position, transform.up, interactDistance, interactableLayer);
+        if (hit.collider != null)
+        {
+            Debug.DrawRay(transform.position, transform.up * interactDistance, Color.red);
+        } else {
+            Debug.DrawRay(transform.position, transform.up * interactDistance, Color.green);
+        }
+        
+    }
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,8 +6,12 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movementVector;
+    private Quaternion rotation;
+    private Vector2 smoothedInput;
+    private Vector2 movementInputSmoothedVelocity;
 
     void Start()
     {
@@ -22,10 +26,30 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movementVector * movementSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rotation);
     }
 
     private void OnMove(InputValue inputValue)
     {
         movementVector = inputValue.Get<Vector2>();
     }
+
+    // Gamepad right stick input.
+    private void OnLook(InputValue inputValue)
+    {
+        smoothedInput = Vector2.SmoothDamp(smoothedInput, inputValue.Get<Vector2>(), ref movementInputSmoothedVelocity, 0.1f);
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, smoothedInput);
+        rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    // Mouse position input.
+    private void OnMouse(InputValue inputValue)
+    {
+        // Set rotation towards mouse position.
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(inputValue.Get<Vector2>());
+        Vector2 direction = mousePosition - rb.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
 }
