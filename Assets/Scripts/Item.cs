02c797687c27da5790fa18b2c1cd1bb01be5da4b
AlbanSdl <alban.delavoreille@free.fr>
2023-07-07 22:10:11 +0200

    add items (but we're not sure if they will be useful)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public static Item Instantiate(ItemType type, Vector2 position) {
+        GameObject obj = Object.Instantiate(Resources.Load("Prefabs/Item") as GameObject, position, Quaternion.identity);
+        Item item = obj.GetComponent<Item>();
+        item.type = type;
+        return item;
+    }
+
+    public ItemType type;
+
+    private void Awake() {
+        GetComponent<SpriteRenderer>().sprite = type.texture;
+    }
+    
+}
---
+++ Assets/Scripts/ItemCraft.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Craft", menuName = "ItemCraft", order = 0)]
+public class ItemCraft : ScriptableObject {
+    public ItemType slot1;
+    public ItemType slot2;
+    public ItemType slot3;
+}
