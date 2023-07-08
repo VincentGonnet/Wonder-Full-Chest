a4f0ad0e009c56ec780520feb6b4c6c7b96818be
Noé Landré <noe.landre@laposte.net>
2023-07-08 07:18:55 +0200

    feat: craft recipes

---
+++ Assets/Scripts/UI/CraftRecipes.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using UnityEngine.Rendering;
+
+public class CraftRecipes : MonoBehaviour
+{
+    [SerializeField] public GameObject prefabGridCell;
+
+    [SerializeField] public GameObject prefabGridText;
+
+    [SerializeField] public GameObject prefabGridImage;
+
+
+    // Start is called before the first frame update
+    void BuildRecipes(ItemCraft[] objs)
+    {
+        // Pass from scale to grid 3*4 with all children
+        float scaleX = this.GetComponent<RectTransform>().sizeDelta.x;
+        float scaleY = this.GetComponent<RectTransform>().sizeDelta.y / 8;
+
+        // Create column items
+        for (int i = 0; i < objs.Length; i++)
+        {
+            ItemCraft foundItem = objs[i];
+
+            GameObject item = Instantiate(prefabGridCell);
+            item.name = "Item" + (i + 1).ToString();
+            item.transform.SetParent(this.transform);
+            RectTransform rt = item.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(scaleX, scaleY);
+            rt.anchorMin = new Vector2(0.5f, 1);
+            rt.anchorMax = new Vector2(0.5f, 1);
+            rt.anchoredPosition = new Vector2(0, (-(scaleY / 2) - scaleY * i));
+
+            GameObject image = Instantiate(prefabGridImage);
+            image.name = "Image";
+            image.transform.SetParent(item.transform);
+            RectTransform rt2 = image.GetComponent<RectTransform>();
+            rt2.anchorMin = new Vector2(0, 0.5f);
+            rt2.anchorMax = new Vector2(0, 0.5f);
+            rt2.anchoredPosition = new Vector2(20, 0);
+            image.GetComponent<Image>().sprite = foundItem.outputTexture;
+
+            int maxI = 3;
+            for (int j = 0; j < foundItem.inputs.Length; j++)
+            {
+                GameObject image2 = Instantiate(prefabGridImage);
+                image2.name = "Image";
+                image2.transform.SetParent(item.transform);
+                RectTransform rt3 = image2.GetComponent<RectTransform>();
+                rt3.anchorMin = new Vector2(1, 0.5f);
+                rt3.anchorMax = new Vector2(1, 0.5f);
+                rt3.anchoredPosition = new Vector2(-20 - (32 * (maxI - j - 1)) - 12 * (maxI - j - 1), 0);
+                image2.GetComponent<Image>().sprite = foundItem.inputs[j].texture;
+
+                if (j != (maxI - 1)) { 
+                    GameObject text = Instantiate(prefabGridText);
+                    text.name = "Text";
+                    text.transform.SetParent(item.transform);
+                    RectTransform rt4 = text.GetComponent<RectTransform>();
+                    rt4.anchorMin = new Vector2(1, 0.5f);
+                    rt4.anchorMax = new Vector2(1, 0.5f);
+                    rt4.anchoredPosition = new Vector2(-43 * (maxI - j - 1), 0);
+                    text.GetComponent<TextMeshProUGUI>().text = "+";
+                    text.GetComponent<TextMeshProUGUI>().fontSize = 10;
+                }
+            }
+        }
+
+    }
+
+}
