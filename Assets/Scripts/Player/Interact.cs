2583d5cc5cc84b36767af242024618d531710389
AlbanSdl <alban.delavoreille@free.fr>
2023-07-08 03:33:32 +0200

    obstacle detection

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    [SerializeField] LayerMask interactableLayer;
-    [SerializeField] float interactDistance = 1f;
-    private RaycastHit2D hit;
-    private GameObject currentInteractable;
-
-    void FixedUpdate()
-    {
-        hit = Physics2D.Raycast(transform.position, transform.up, interactDistance, interactableLayer);
-        if (hit.collider != null)
-        {
-            Debug.DrawRay(transform.position, transform.up * interactDistance, Color.red);
-            currentInteractable = hit.collider.gameObject;
-        } else {
-            Debug.DrawRay(transform.position, transform.up * interactDistance, Color.green);
+    public float detectionDistance = 5f;
+    
+    private void LateUpdate() {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.right, this.detectionDistance, LayerMask.GetMask("Obstacles"));
+        if (hit.collider != null) {
+            Debug.Log("Start QTE Challenge here !");
         }
     }
 
-    private void OnInteract()
-    {
-        if (currentInteractable != null)
-        {
-            Debug.Log("Interacting with " + currentInteractable.name);
-        }
+    private void OnDrawGizmos() {
+        Gizmos.DrawRay(this.transform.position, Vector2.right * this.detectionDistance);
     }
 }
